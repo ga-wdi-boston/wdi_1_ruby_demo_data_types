72282e5b8d016aafd0c7b928422399b420ac46ee
David Fisher <tibbon@gmail.com>
2015-01-11 18:08:54 -0500

    Tests written to backup true and false

--- spec/boolean_example_spec.rb
+++ spec/boolean_example_spec.rb
@@ -1,1 +1,35 @@
-require_relative '../lib/boolean_example'
+RSpec.describe TrueClass do
+  it "is true" do
+    expect(true).to be true
+  end
+
+  it 'is false when one bang is used' do
+    expect(!true).to be false
+  end
+
+  it 'is true when two bangs are used' do
+    expect(!!true).to be true
+  end
+
+  it 'is false when three bangs are used' do
+    expect(!!!true).to be false
+  end
+end
+
+RSpec.describe FalseClass do
+  it "is false" do
+    expect(false).to be false
+  end
+
+  it 'is true when one bang is used' do
+    expect(!false).to be true
+  end
+
+  it 'is false when two bangs are used' do
+    expect(!!false).to be false
+  end
+
+  it 'is true when three bangs are used' do
+    expect(!!!false).to be true
+  end
+end
